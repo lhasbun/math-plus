6d68492434455acf053c3f326df938f29103ffe1
Luis Hasbun <41136478+lhasbun@users.noreply.github.com>
2025-04-02 20:13:04 -0500

    Update geometry.py
    Add 'Triangle' class, remove function calls from constructor

--- src/math_plus/geometry.py
+++ src/math_plus/geometry.py
@@ -10,9 +10,6 @@ class Square:
 
     def __init__(self, side_length):
         self.side_length = side_length
-        self.area = self.area()
-        self.perimeter = self.perimeter()
-        self.diagonal = self.diagonal()
 
     def area(self) -> float:
         """Calculate the area of the square.\n
@@ -30,7 +27,10 @@ class Square:
         return self.side_length * math.sqrt(2)
     
 class Rectangle:
-    """"A class to represent a rectangle.\nAttributes:\n length (float): The length of the rectangle.\n"""
+    """"A class to represent a rectangle.\nAttributes:\n 
+    length (float): The length of the rectangle.\n 
+    width (float): The width of the rectangle.\n"""
+
     def __init__(self, length, width):
         self.length = length
         self.width = width
@@ -49,3 +49,38 @@ class Rectangle:
         """Calculate the length of the diagonal of the rectangle.\n
         Formula: Diagonal = √(length^2 + width^2)"""
         return math.sqrt(self.length ** 2 + self.width ** 2)
+    
+class Triangle:
+    """A class to represent a triangle.\nAttributes:\n 
+    side_a (float): The length of side a of the triangle.\n 
+    side_b (float): The length of side b of the triangle.\n 
+    side_c (float): The length of side c of the triangle.\n"""
+
+    def __init__(self, side_a, side_b, side_c):
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
+    def area(self) -> float:
+        """Calculate the area of the triangle.\n
+        Formula: Using Heron's formula: Area = √[s(s - a)(s - b)(s - c)] where ( s = (a + b + c)/2 )"""
+        s = ((self.side_a + self.side_b + self.side_c) / 2 )
+        return math.sqrt(s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c))
+
+    def perimeter(self) -> float:
+        """Calculate the perimeter of the triangle.\n
+        Formula: Perimeter = a + b + c"""
+        return self.side_a + self.side_b + self.side_c
+    
+    def pythagorean_theorem(self) -> bool:
+        """Check if the triangle satisfies the Pythagorean theorem.\n
+        Formula: a^2 + b^2 = c^2"""
+        # Sort the sides to identify the longest side
+        sides = sorted([self.side_a, self.side_b, self.side_c])
+        self.side_a, self.side_b, self.side_c = sides
+        return (self.side_a ** 2 + self.side_b ** 2 == self.side_c ** 2)
+    
+    def law_of_cosines(self) -> float:
+        """Calculate the angle opposite to side c using the law of cosines.\n
+        Formula: c^2 = a^2 + b^2 - 2ab * cos(C) => C = cos^-1((a^2 + b^2 - c^2) / (2ab))"""  
+        return math.acos((self.side_a ** 2 + self.side_b ** 2 - self.side_c ** 2) / (2 * self.side_a * self.side_b))
